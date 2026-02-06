38b63800ea62d4d054972d0af3e6a6c7de5a6a45
WebbCouture <lesnik.amanda@icloud.com>
2026-02-06 23:03:47 +0100

    Add base template and custom allauth templates

--- products/views.py
+++ products/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello — this is the homepage.")
-
+    return render(request, "home/index.html")
