412b32760649b458d92d77fc543d17eb924a4960
WebbCouture <lesnik.amanda@icloud.com>
2026-02-10 00:17:16 +0100

    Add checkout models and apply migrations

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,16 +1,50 @@
 from django.contrib import admin
-from .models import Order, OrderItem
+from .models import Order, OrderLineItem
 
 
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    extra = 0
-    readonly_fields = ("line_total",)
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ("lineitem_total",)
 
 
-@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("id", "email", "paid", "created_at")
-    list_filter = ("paid", "created_at")
-    search_fields = ("email", "full_name")
-    inlines = (OrderItemInline,)
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = (
+        "order_number",
+        "date",
+        "delivery_cost",
+        "order_total",
+        "grand_total",
+    )
+
+    fields = (
+        "order_number",
+        "date",
+        "full_name",
+        "email",
+        "phone_number",
+        "country",
+        "postcode",
+        "town_or_city",
+        "street_address1",
+        "street_address2",
+        "county",
+        "delivery_cost",
+        "order_total",
+        "grand_total",
+    )
+
+    list_display = (
+        "order_number",
+        "date",
+        "full_name",
+        "order_total",
+        "delivery_cost",
+        "grand_total",
+    )
+
+    ordering = ("-date",)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,42 +1,157 @@
-from django.conf import settings
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
 from products.models import Product
 
 
 class Order(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name="orders",
+    order_number = models.CharField(
+        max_length=32,
+        null=False,
+        editable=False
     )
-    full_name = models.CharField(max_length=200)
-    email = models.EmailField()
-    address1 = models.CharField(max_length=255)
-    address2 = models.CharField(max_length=255, blank=True)
-    city = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=20)
-    country = models.CharField(max_length=100)
+    full_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+    )
+    email = models.EmailField(
+        max_length=254,
+        null=False,
+        blank=False
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False
+    )
+    country = models.CharField(
+        max_length=40,
+        null=False,
+        blank=False
+    )
+    postcode = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+    town_or_city = models.CharField(
+        max_length=40,
+        null=False,
+        blank=False
+    )
+    street_address1 = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False
+    )
+    street_address2 = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
+    county = models.CharField(
+        max_length=80,
+        null=True,
+        blank=True
+    )
 
-    stripe_pid = models.CharField(max_length=255, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    paid = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Order #{self.id} - {self.email}"
+    delivery_cost = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
+    grand_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+    )
 
+    def _generate_order_number(self):
+        """Generate a random, unique order number using UUID"""
+        return uuid.uuid4().hex.upper()
 
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-    quantity = models.PositiveIntegerField(default=1)
-    line_total = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    def update_total(self):
+        """Update grand total each time a line item is added"""
+        self.order_total = self.lineitems.aggregate(
+            Sum("lineitem_total")
+        )["lineitem_total__sum"] or 0
+
+        self.delivery_cost = (
+            self.order_total
+            * settings.STANDARD_DELIVERY_PERCENTAGE
+            / 100
+        )
+
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
 
     def save(self, *args, **kwargs):
-        if self.product:
-            self.line_total = self.product.price * self.quantity
+        """Set order number if it hasn't been set already"""
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.quantity} x {self.product}"
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name="lineitems"
+    )
+    product = models.ForeignKey(
+        Product,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
+    )
+    product_size = models.CharField(
+        max_length=2,
+        null=True,
+        blank=True
+    )
+    quantity = models.IntegerField(
+        null=False,
+        blank=False,
+        default=0
+    )
+    lineitem_total = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=False,
+        blank=False,
+        editable=False
+    )
+
+    def save(self, *args, **kwargs):
+        """Set lineitem total and update order"""
+        self.lineitem_total = self.product.price * self.quantity
+
+        super().save(*args, **kwargs)
+
+        self.order.update_total()
+
+    def __str__(self):
+        return (
+            f"SKU {self.product.sku} "
+            f"on order {self.order.order_number}"
+        )
